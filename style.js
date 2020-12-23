8bd66f7a489058485512c9702f909b0a30b73432
LouiseGutenberg <74434270+LouiseGutenberg@users.noreply.github.com>
2020-12-23 16:19:16 -0700

    Update style.js
    didn't like the previous buttons. trying a new function for a collapsible class

--- style.js
+++ style.js
@@ -1,8 +1,14 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
 }
